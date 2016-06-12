6fda8b95482547cbce0b9a7c08d8a24ea1e0e4fa
Han Koh <rhgksrua@gmail.com>
2016-06-12 14:59:36 -0500

    Add project files.

---
+++ Grades/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeBook book = new GradeBook();
+
+            
+
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+
+            book.WriteGrades(Console.Out);
+
+            GradeStatistics stats = book.ComputeStatistics();
+            WriteResult("Average", stats.AverageGrade);
+            WriteResult("Highest", (int)stats.HighestGrade);
+            WriteResult("Lowest", stats.LowestGrade);
+            WriteResult(stats.Description, stats.LetterGrade);
+
+        }
+        static void WriteResult(string description, int result)
+        {
+            Console.WriteLine(description + ": " + result);
+        }
+        static void WriteResult(string description, float result)
+        {
+            Console.WriteLine("{0}: {1:F}", description, result);
+        }
+        static void WriteResult(string description, string result)
+        {
+            Console.WriteLine("{0}: {1}", description, result);
+        }
+        
+    }
+}
